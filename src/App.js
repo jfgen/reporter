722c668ca734cfc2473be0494a700956927a9c81
jorge <jorge@portugalwebdesign.pt>
2018-03-24 21:27:53 +0000

    new design
    It now communicates with the API

--- src/App.js
+++ src/App.js
@@ -1,41 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
+import React from 'react';
+import _ from 'lodash';
 import './App.css';
 
 const location = require('@derhuerst/browser-location');
 
-class App extends Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            coords: {latitude: null, longitude: null}
-        };
+            status: 'idle'
+        }
 
         this.sendData = this.sendData.bind(this);
     }
 
     sendData() {
-        location((err, loc) => {
-            if (err) console.error(err);
-            else {
-                this.setState({coords: {latitude: loc.latitude, longitude: loc.longitude}});
+        location((err, loc)=> {
+            if(err) {
+                throw err;
             }
-        })
-    }
+            const API_URL = 'https://shiftfaroapp-api.herokuapp.com';
+            const pooBody = {
+                "numberOfPoos": 1000,
+                "latlng": [loc.longitude, loc.latitude],
+                "pooIndicator": _.random(0,10),
+                "date": ''
+            };
+            const init = {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                mode: 'CORS',
+                body: JSON.stringify(pooBody)
+            };
+
+            fetch(`${API_URL}/api/databaseFiles`, init)
+                .then(() => {
+                    this.setState({status: 'success'});
+                })
+                .catch(function (err) {console.log(err)})
+        });
+    };
 
   render() {
     return (
       <div className="app">
-        <header className="header">
-          <img src={logo} className="logo" alt="logo" />
-          <h1 className="App-title">Poo Spotter</h1>
-        </header>
+
         <div className="content">
-            <p className="app-intro">
-               Help keeping your city clean by reporting all crappy findings!
-            </p>
-            <button className="report-btn" onClick={this.sendData}>Report crap</button>
+            <h1 className="title">Help your city!</h1>
+            {this.state.status !== 'success'
+                ?
+                    <div>
+                        <p className="app-intro">
+                        Help keeping your city clean by reporting all crappy findings!
+                        </p>
+                        <button className="report-btn" onClick={this.sendData}>Report crap</button>
+                    </div>
+                :
+                    <div className="success-area">
+                        <p className="app-intro">
+                            Thanks for your input, you first class citizen you ;)
+                        </p>
+
+                    </div>
+
+            }
+
         </div>
       </div>
     );
